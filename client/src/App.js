9e7c23edbbf704dc4ff7fc777408abf9351ef253
davjdo <davido_204@hotmail.com>
2019-04-01 22:26:03 +0200

    add redux config feature with auth and error actions and reducers

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,28 @@ import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 import ItemModal from './components/ItemModal';
 import { Container } from 'reactstrap';
+import store from './store';
+import { loadUser } from './actions/authActions';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AppNavbar />
-        <Container>
-          <ItemModal />
-          <ShoppingList />
-        </Container>
-      </div>
-    );
-  }
+	componentDidMount() {
+		store.dispatch(loadUser());
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<AppNavbar />
+				<Container>
+					<ItemModal />
+					<ShoppingList />
+				</Container>
+			</div>
+		);
+	}
 }
 
 export default App;
